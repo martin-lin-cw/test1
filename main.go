ffd8aeab9972d676ca9263b836815441a23dad31
martin-lin-cw <martin6061097@gmail.com>
2024-07-12 16:55:12 +0900

    update proto definition

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net"
+
+	test1pb "github.com/martin-lin-cw/test1/gen/proto/test1/v1"
+	"google.golang.org/grpc"
+)
+
+type Test1Service struct {
+	test1pb.UnsafeTest1ServiceServer
+}
+
+// Hello1 implements test1pb.Test1ServiceServer.
+func (t *Test1Service) Hello1(context.Context, *test1pb.Hello1Request) (*test1pb.Hello1Response, error) {
+	return &test1pb.Hello1Response{Result: "Hello1"}, nil
+}
+
+func NewTest1Service() test1pb.Test1ServiceServer {
+	return &Test1Service{}
+}
 
 func main() {
 	fmt.Println("Hello, test1")
+
+	server := grpc.NewServer()
+
+	test1Server := NewTest1Service()
+
+	test1pb.RegisterTest1ServiceServer(server, test1Server)
+
+	ln, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	server.Serve(ln)
 }
